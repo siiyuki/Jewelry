ca65ffbc4546b390d0c8f378cbabaf8c9e228210
irosomen <changotsu.1222@gmail.com>
2022-12-26 08:11:03 +0900

    ThankYouStageの変更(Playするとカメラがリセットされる)

---
+++ Assets/TaiseiGoto/Scripts/ChangeColorBlue.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+public class ChangeColorBlue : MonoBehaviour
+{
+    /// <summary>
+    /// 衝突した時
+    /// </summary>
+    /// <param name="collision"></param>
+    void OnCollisionEnter(Collision collision)
+    {
+        // 衝突した相手にPlayerタグが付いているとき
+        if (collision.gameObject.tag == "ChangeColor")
+        {
+            // 自分自身の色を赤に変える
+            GetComponent<Renderer>().material.color = Color.blue;
+        }
+    }
+}
